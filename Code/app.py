fd6037b9b13658c88011cdd601c4e6eaa0dc04f4
Sofia Scribner <sofiadscribner@gmail.com>
2025-04-15 11:27:08 -0600

    added tabs 2 and 3

--- Code/app.py
+++ Code/app.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import kaleido
 import streamlit as st
 
@@ -46,4 +47,89 @@ with tab1:
 
     fig.update_layout(coloraxis_showscale=False)
 
+    st.plotly_chart(fig, use_container_width=True)
+
+with tab2:
+    input = st.radio('Select a marketing metric.', ['Brand Awareness','Brand Familiarity',  'Brand Momentum', 'Consideration of Purchasing','Perception of Quality'])
+    metric_map = {
+            'Brand Awareness': 'awareness_increase',
+            'Brand Familiarity': 'familiarity_increase',
+            'Brand Momentum': 'momentum_increase',
+            'Consideration of Purchasing': 'consideration_increase',
+            'Perception of Quality': 'quality_increase'
+        }
+    
+    stat_of_interest = metric_map[input]
+
+    # Filter data
+    filtered_df = df[df[stat_of_interest] != 0]
+
+    if filtered_df.empty:
+        st.write("No data available for the selected metric!")
+    else:
+        # Sort and process data
+        filtered_df = filtered_df.sort_values(by=stat_of_interest, ascending=False)
+        brands = filtered_df['Advertiser/product']
+        scores = filtered_df[stat_of_interest]
+
+        fig = go.Figure()
+        for i, brand in enumerate(brands):
+            fig.add_trace(go.Scatter(
+                x=[brand, brand],
+                y=[0, scores.iloc[i]],
+                mode='lines+markers',
+                line=dict(color='gray', width=2),
+                marker=dict(size=[0, 15], color='red'),
+                showlegend=False,
+                name=brand
+            ))
+
+        fig.update_layout(
+            title=f'Top 10 {input} Increases',
+            xaxis_title='Brand',
+            yaxis_title= 'Point Increase',
+            xaxis_tickangle=-25,
+            plot_bgcolor='white',
+            height=600
+        )
+
+        st.plotly_chart(fig, use_container_width=True)
+
+with tab3:
+    df_table = df[['Advertiser/product', 'Likes', 'Views']].copy().dropna()
+    df_table['like_to_view_ratio'] = (df_table['Likes'] / df_table['Views']) * 100
+    df_table['like_to_view_ratio'] = df_table['like_to_view_ratio'].round(2)
+
+    selection = st.selectbox('Sort by:', ['Likes', 'Views', 'Like-to-View-Ratio'])
+
+    if selection == 'Likes':
+        sort_by = 'Likes'
+    elif selection == 'Views':
+        sort_by = 'Views'
+     elif selection == 'Likes':
+        sort_by = 'like_to_view_ratio'
+
+    df_table = df_table.sort_values(by= sort_by, ascending=False)
+
+    fig = go.Figure(data=[go.Table(
+        header=dict(
+            values=["Brand", "Likes", "Views", "Like-to-View Ratio (%)"],
+            fill_color='red',
+            align='center',
+            font=dict(color='white', size=14)
+        ),
+        cells=dict(
+            values=[
+                df_table['Advertiser/product'],
+                df_table['Likes'],
+                df_table['Views'],
+                df_table['like_to_view_ratio']
+            ],
+            fill_color='white',
+            align='center',
+            font=dict(size=12)
+        ))
+    ])
+
+    fig.update_layout(title='Youtube Engagement Table')
     st.plotly_chart(fig, use_container_width=True)
